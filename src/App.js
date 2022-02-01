a8413d51fac8422af71831af6dd7c898d7c181d1
Harrison Reich <hreich1998@gmail.com>
2022-02-01 14:10:38 -0800

    added components to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 
+// Components
+import About from './components/About'
+import Contact from './components/Contact'
+import Portfolio from './components/Portfolio'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 function App() {
   return (
     <>
-    
-    
+    <Header />
+    <About />
+    <Portfolio />
+    <Contact />
+    <Footer />
     </>
   );
 }
