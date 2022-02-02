5fd69458244f5f16e0d008dced439af7c066abce
Harrison Reich <hreich1998@gmail.com>
2022-02-01 17:00:31 -0800

    footer built

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,26 @@
-const footer = () => {
-  return (
-    <>
+import Box from "@mui/material/Box"
 
-    </>
+const AppFooter = (props) => {
+  let footerStyle = {
+    display: "grid",
+    gridTemplateColumns: "repeat(2, 1fr)",
+    borderTop: "1px solid #AAA",
+    borderBottom: "1px solid #AAA",
+    marginTop: "35em",
+    bgcolor: "primary.main",
+    color: "white",
+    width: "100%",
+    padding: "5px",
+  }
+  return (
+    <Box sx={footerStyle}>
+      <div>&nbsp;</div>
+      <div>
+        <strong textalign="center">Harrison Reich</strong><br />
+        <strong textalign="center">React Portfolio</strong>
+      </div>
+    </Box>
   )
 }
 
-export default footer
+export default AppFooter;
