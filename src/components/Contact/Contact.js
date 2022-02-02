406bbd7bac8d526321cd4fc93eb031558b0a4d55
Harrison Reich <hreich1998@gmail.com>
2022-02-02 11:10:49 -0800

    card size

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -10,10 +10,10 @@ const contact = () => {
   return (
     <>
       <Grid>
-        <Card style={{ maxWidth: 1000, margin: '0 auto' }}>
+        <Card style={{ maxWidth: 765, margin: '0 auto' }}>
           <CardContent>
             <Typography align="center" gutterBottom variant='h5'>
-              Please fill out the contact card below to get in touch about work or a project!
+              Fill out the contact card below to get in touch about work or a project!
             </Typography>
             <Typography align="center" variant='body2' color='textSecondary' component='p' gutterBottom>
               You can also find my contact information on LinkedIn/Github
