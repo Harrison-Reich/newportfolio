04e519210b8d87f479a52791f7f2f027eb25bb50
Harrison Reich <hreich1998@gmail.com>
2022-02-02 14:01:34 -0800

    code

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -13,7 +13,7 @@ const About = () => {
     <>
       <Grid>
         <Container>
-          <Box marginTop={25}>
+          <Box marginTop={27}>
             <Typography fontFamily="-apple-system" color="white" gutterBottom variant='h1' align="center">
               Harrison R. Reich's
             </Typography>
@@ -28,22 +28,22 @@ const About = () => {
             <Grid container columnSpacing={{ xs: 1 }}>
               <Grid item xs={6}>
                 <Item>
-                  <Box sx={{ maxWidth: 300 }} >
+                  <Box sx={{ maxWidth: 400 }} >
                     <Card>
                       <CardMedia
                         component="img"
-                        height="300"
+                        height="400"
                         image="https://media-exp1.licdn.com/dms/image/C5603AQGsruXwe9BMCg/profile-displayphoto-shrink_400_400/0/1604837439389?e=1649289600&v=beta&t=A3Bvmw8NwIzcekx1HuoT22cyM15COFD9vfVgh7aW0XQ">
                       </CardMedia>
-                    </Card>
+                    </Card>3
                   </Box>
                 </Item>
               </Grid>
               <Grid item xs={6}>
                 <Item>
-                    <Typography fontFamily="" color="white" gutterBottom variant='h5' align="left">
-                      Hello! My name is Harrison Reich and welcome to my portfolio website! I am a UCI Bootcamp student learning how to become a Full Stack Web Developer! I have a background in graphic design and aim to make eye catching, easy to use websites! Take a look at some of my work in the Projects section below. If you have any questions, please contact me using the form at the bottom!
-                    </Typography>
+                  <Typography fontFamily="" color="white" gutterBottom variant='h5' align="left">
+                    Hello! My name is Harrison Reich and welcome to my portfolio website! I am a UCI Bootcamp student learning how to become a Full Stack Web Developer! I have a background in graphic design and aim to make eye catching, easy to use websites! Take a look at some of my work in the Projects section below. If you have any questions, please contact me using the form at the bottom!
+                  </Typography>
                 </Item>
               </Grid>
             </Grid>
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,7 +1,28 @@
+import * as React from 'react';
+import Grid from '@mui/material/Grid';
+import Item from '@mui/material/ListItem';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import CardActions from '@mui/material/CardActions';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
+
 const portfolio = () => {
   return (
     <>
-
+      <Grid>
+        <Container>
+          <Box marginTop={25}>
+            <Typography fontFamily="-apple-system" color="white" gutterBottom variant='h2' align="left">
+              Projects
+            </Typography>
+          </Box>
+        </Container>
+      </Grid>
     </>
   )
 }
